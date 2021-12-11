3b68ca1523c3597b6aa22eff4964aac8ada860ee
Kara Constantakos <64597989+KaraConstantakos5181@users.noreply.github.com>
2021-12-11 00:06:34 -0700

    changes

--- final/index.js
+++ final/index.js
@@ -16,11 +16,11 @@ const photos2 = ["assets/projects/sec1.png","assets/projects/sec2.png","assets/p
 
 let photocount2 = 0;
 
-document.getElementById("2newphoto").addEventListener("click", buttonClicked2);
+document.getElementById("newphoto2").addEventListener("click", buttonClicked2);
 
 function buttonClicked2(){
   console.log(photos2[photocount2]);
-  document.getElementById("2imgsection").innerHTML = "<img id='2landPhoto' class='image' src=" + photos2[photocount2] + " ' >";
+  document.getElementById("imgsection2").innerHTML = "<img id='landPhoto3' class='image' src=" + photos2[photocount2] + " ' >";
   if(photocount2 >= 3){
     photocount2 = 0;
   }else{photocount2 ++;}
@@ -29,11 +29,11 @@ function buttonClicked2(){
 const photos3 = ["assets/projects/thir1.JPG","assets/projects/thir2.JPG","assets/projects/3.jpg"];
 let photocount3 = 0;
 
-document.getElementById("3newphoto").addEventListener("click", buttonClicked3);
+document.getElementById("newphoto3").addEventListener("click", buttonClicked3);
 
 function buttonClicked3(){
   console.log(photos3[photocount3]);
-  document.getElementById("3imgsection").innerHTML = "<img id='3landPhoto' class='image' src=" + photos3[photocount3] + " ' >";
+  document.getElementById("imgsection3").innerHTML = "<img id='landPhoto3' class='image' src=" + photos3[photocount3] + " ' >";
   if(photocount3 >= 2){
     photocount3 = 0;
   }else{photocount3 ++;}
