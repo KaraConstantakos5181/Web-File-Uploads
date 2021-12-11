cfb288d2a5a19a61f2c30df7858c1c857a5e4ffd
Kara Constantakos <64597989+KaraConstantakos5181@users.noreply.github.com>
2021-12-10 23:56:35 -0700

    done

--- final/index.js
+++ final/index.js
@@ -1,13 +1,40 @@
-const photos = ["assets/projects/first1.jpg","assets/projects/first2.jpg","assets/projects/first3.jpg","assets/projects/first4.jpg","assets/projects/first5.jpg"];
+const photos = ["assets/projects/first2.jpg","assets/projects/first3.jpg","assets/projects/first4.jpg","assets/projects/first5.jpg","assets/projects/1.jpg"];
 
 let photocount = 0;
+document.getElementById("newphoto").addEventListener("click", buttonClicked1);
 
-document.getElementById("newphoto").addEventListener("click", buttonClicked);
-
-function buttonClicked(){
+function buttonClicked1(){
   console.log(photos[photocount]);
-  document.getElementById("imgsection").innerHTML = "<img id='qtext' class='image' src=" + photos[photocount] + " ' >";
+  document.getElementById("imgsection").innerHTML = "<img id='landPhoto' class='image' src=" + photos[photocount] + " ' >";
   if(photocount >= 4){
     photocount = 0;
   }else{photocount ++;}
+}
+
+
+const photos2 = ["assets/projects/sec1.png","assets/projects/sec2.png","assets/projects/sec3.png","assets/projects/2.png"];
+
+let photocount2 = 0;
+
+document.getElementById("2newphoto").addEventListener("click", buttonClicked2);
+
+function buttonClicked2(){
+  console.log(photos2[photocount2]);
+  document.getElementById("2imgsection").innerHTML = "<img id='2landPhoto' class='image' src=" + photos2[photocount2] + " ' >";
+  if(photocount2 >= 3){
+    photocount2 = 0;
+  }else{photocount2 ++;}
+}
+
+const photos3 = ["assets/projects/thir1.JPG","assets/projects/thir2.JPG","assets/projects/3.jpg"];
+let photocount3 = 0;
+
+document.getElementById("3newphoto").addEventListener("click", buttonClicked3);
+
+function buttonClicked3(){
+  console.log(photos3[photocount3]);
+  document.getElementById("3imgsection").innerHTML = "<img id='3landPhoto' class='image' src=" + photos3[photocount3] + " ' >";
+  if(photocount3 >= 2){
+    photocount3 = 0;
+  }else{photocount3 ++;}
 }
